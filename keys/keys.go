9aed3f8f82e9db3891bcacf234fedc497779e4f1
Stan (Konstantin) <kostyacherepanov1@gmail.com>
2025-09-14 14:49:38 +0700

    added keys

---
+++ keys/keys.go
@@ -0,0 +1,50 @@
+package keys
+
+import "fmt"
+
+const (
+	OneMinute   = "1m"
+	FiveMinutes = "5m"
+	OneHour     = "1h"
+	EightHours  = "8h"
+)
+
+func GetLastOiKey(symbol string) string {
+	return fmt.Sprintf("mkt:last:oi:%s", symbol)
+}
+
+func GetLastFundingKey(symbol string) string {
+	return fmt.Sprintf("mkt:last:funding:%s", symbol)
+}
+
+func GetFundingKey(symbol string, suffix string) string {
+	return fmt.Sprintf("mkt:funding:%s:%s", suffix, symbol)
+}
+
+func GetOiKey(symbol string, suffix string) string {
+	return fmt.Sprintf("mkt:oi:%s:%s", suffix, symbol)
+}
+
+func GetKlineKey(symbol string, suffix string) string {
+	return fmt.Sprintf("mkt:kline:%s:%s", suffix, symbol)
+}
+
+func GetLastKlineKey(symbol string, suffix string) string {
+	return fmt.Sprintf("mkt:last:kline:%s:%s", suffix, symbol)
+}
+
+func GetTsID(ts int64) string {
+	return fmt.Sprintf("%d-0", ts)
+}
+
+func GetLiqRawKey(symbol string) string {
+	return fmt.Sprintf("mkt:liqs:raw:%s", symbol)
+}
+
+func GetLiq1mKey(symbol string) string {
+	return fmt.Sprintf("mkt:liqs:1m:%s", symbol)
+}
+
+func GetLastLiq1mKey(symbol string) string {
+	return fmt.Sprintf("mkt:last:liqs:1m:%s", symbol)
+}
